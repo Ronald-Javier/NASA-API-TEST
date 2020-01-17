a02b02f6bf76604ee358bae8d241c729c9b17a02
ronaldjavier <ronald2807@hotmail.com>
2020-01-17 16:50:23 -0300

    Definiendo el El Build Web Page

--- nasa_api.rb
+++ nasa_api.rb
@@ -13,3 +13,35 @@ def request(url, api_key)
     response = http.request(request)
     JSON.parse(response.read_body)
 end
+
+
+def buid_web_page(step1)
+
+    clean_data = step1['photos']    
+    photo_info = {}                     
+    photo_src = []                      
+    
+    clean_data.each do |i|              
+        photo_info = i
+        photo_src.push(photo_info['img_src']) 
+    end
+
+   
+   web1 = File.open('photos.html', 'w') do |f|
+   web2 = File.open('style.css', 'w') do |x|
+
+    x.puts css_body
+
+        f.puts head                     
+    
+            f.puts "<h1 class='text-light'>FOTOS DE LA NASA!</h1>
+            <ul>"
+            photo_src.each do |i|
+            f.puts "<li><img src='#{i}' width='900' alt='Nasa Photos'></li>"
+            f.puts '</ul><br>'
+        end
+
+        f.puts footer                   
+    end
+end
+end
